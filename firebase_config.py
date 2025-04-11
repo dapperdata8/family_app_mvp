d3834d3d46232f365b8a33e7694bb9ac15bad870
Bobby Rountree <brountree@mrdapperdata.com>
2025-04-10 22:56:11 -0400

    Switch to st.secrets for Streamlit Cloud

--- firebase_config.py
+++ firebase_config.py
@@ -1,12 +1,26 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
+import streamlit as st
 
 def init_firebase():
-    cred = credentials.Certificate("firebase_key.json")
+    firebase_config = {
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"],
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+        "universe_domain": st.secrets["firebase"]["universe_domain"]
+    }
 
     if not firebase_admin._apps:
+        cred = credentials.Certificate(firebase_config)
         firebase_admin.initialize_app(cred, {
-            'storageBucket': 'familyappmvp-dapper.appspot.com'
+            'storageBucket': f"{firebase_config['project_id']}.appspot.com"
         })
 
     db = firestore.client()
